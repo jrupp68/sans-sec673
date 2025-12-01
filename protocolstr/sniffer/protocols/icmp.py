90025c873c1a37630c48923aa7b1b7ac5939f616
jrupp <jrupp@acuity.com>
2025-12-01 16:57:53 -0600

    solve icmp protocolstr

--- protocolstr/sniffer/protocols/icmp.py
+++ protocolstr/sniffer/protocols/icmp.py
@@ -1,6 +1,10 @@
 from sniffer.protocols.protocol import Protocol
 
+
 class ICMP(Protocol):
-    header_format = '!BBH'
+    header_format = "!BBH"
     header_length = 4
-    header_fields = ["icmp_type","icmp_code","icmp_chksum"]
+    header_fields = ["icmp_type", "icmp_code", "icmp_chksum"]
+
+    def __str__(self):
+        return f"Type: {self.parsed['icmp_type']} Code: {self.parsed['icmp_type']}"
