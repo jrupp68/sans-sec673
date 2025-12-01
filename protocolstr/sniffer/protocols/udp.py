3c1b1aad6a67be5dfd741338bf11326e58e29c1d
jrupp <jrupp@acuity.com>
2025-12-01 16:52:20 -0600

    solve udp protolstr

--- protocolstr/sniffer/protocols/udp.py
+++ protocolstr/sniffer/protocols/udp.py
@@ -1,6 +1,12 @@
 from sniffer.protocols.protocol import Protocol
 
+
 class UDP(Protocol):
-    header_format = '!HHHH'
+    header_format = "!HHHH"
     header_length = 8
-    header_fields = ["udp_sport","udp_dport","udp_len","udp_chksum"]
+    header_fields = ["udp_sport", "udp_dport", "udp_len", "udp_chksum"]
+
+    def __str__(self):
+        return (
+            f"UDP Port {self.parsed['udp_sport']} > UDP Port {self.parsed['udp_dport']}"
+        )
