9810d587cd99cae2e6bc570d60df834bc27b5729
sec673 <673@673.local>
2025-12-02 14:35:54 -0500

    added packet work

--- packet/sniffer/packet.py
+++ packet/sniffer/packet.py
@@ -1,3 +1,35 @@
+from collections import UserList
+from sniffer.protocols import Protocol
 
-class Packet(list):
-    pass
+class Packet(UserList):
+    
+    def __init__(self, initlist=None):
+        self.data = []
+        if initlist:
+            if type(initlist) == list():
+                self.data.append(initlist)
+
+    def __setitem__(self, pos, data):
+        if type(data) != Protocol:
+            print('Packet can only contains Protocols')
+            raise TypeError
+        self.data[pos] = data
+
+    def append(self, data):
+        if type(data) != Protocol:
+            print('Packet can only contains Protocols')
+            raise TypeError
+        self.data.append(data)
+
+    def extend(self, data):
+        for item in data:
+            if type(item) != Protocol:
+                print('Packet can only contain Protocols')
+                raise TypeError
+            self.data.append(item)
+
+    def insert(self, pos, data):
+        if type(data) != Protocol:
+            print('Packet can only contains Protocols')
+            raise TypeError
+        self.data.insert(pos, data)
