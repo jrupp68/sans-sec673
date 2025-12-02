aab2ad0ce5bd5fcd7d86b6d7001b350d01649a0f
sec673 <673@673.local>
2025-12-02 12:44:28 -0500

    solve tcp

--- tcp/sniffer/protocols/tcp.py
+++ tcp/sniffer/protocols/tcp.py
@@ -1,17 +1,21 @@
-# Create your TCP class here.
+from sniffer.protocols.protocol import Protocol
+import itertools
 
-# You can use a header format of '!HHIIBBHHH'
-# The correct header length is 20
-# The TCP Header field names are ["tcp_sport","tcp_dport","tcp_seq","tcp_ack","tcp_hlen","tcp_flag","tcp_window","tcp_chksum","tcp_urg"]
+class TCP(Protocol):
+    header_format = '!HHIIBBHHH'
+    header_length = 20
+    header_fields = ["tcp_sport","tcp_dport","tcp_seq","tcp_ack","tcp_hlen","tcp_flag","tcp_window","tcp_chksum","tcp_urg"]
 
-# You can use this port lookup table for the tcp_sportstr and tcp_dportstr fields
-port_lookups = {80:'http', 443:'https', 22:'ssh'}
-
-#You can convert this function taken from the 573 sniffer into a method and use it to convert tcp_flags to a string.
-# def tcp_flags_as_str(flag):
-#     file_flags = ['CWR',  'ECE', 'URG', 'ACK', 'PSH', 'RST', 'SYN', 'FIN']
-#         return "|".join(list(itertools.compress(file_flags,map(int,format(flag,"08b")))))
-
-
+    def process(self,bytes_in):
+        port_lookups = {80:'http', 443:'https', 22:'ssh'}
+        super().process(bytes_in)
+        self.parsed['tcp_flagstr'] = self.tcp_flags_as_str(self.parsed['tcp_flag'])
+        self.parsed['tcp_sportstr'] = port_lookups.get(self.parsed['tcp_sport'])  
+        self.parsed['tcp_dportstr'] = port_lookups.get(self.parsed['tcp_dport'])
 
 
+    @staticmethod
+    def tcp_flags_as_str(flag):
+        file_flags = ['CWR',  'ECE', 'URG', 'ACK', 'PSH', 'RST', 'SYN', 'FIN']
+        return "|".join(list(itertools.compress(file_flags,map(int,format(flag,"08b")))))
+    
