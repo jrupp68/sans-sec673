9c1f33dfb3d852d5a798a194614d6bf34889992d
sec673 <673@673.local>
2025-12-03 18:00:06 -0500

    solve bonus-namedintegers

--- bonus-namedintegers/solution.py
+++ bonus-namedintegers/solution.py
@@ -1,2 +1,11 @@
 class NamedInteger(int):
-    pass
+    def __new__(cls, name, *args, **kwargs):
+        instance = super().__new__(cls, *args, **kwargs)
+        instance.name = name
+        return instance
+
+    def as_tuple(self):
+        return (self.name, self.real)
+
+    def __repr__(self):
+        return f"NamedInteger('{self.name}', {self.real})"
