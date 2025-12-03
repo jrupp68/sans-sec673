7c04940c15ba0f47d378fc86d7a7f55aa20a847b
sec673 <673@673.local>
2025-12-03 10:17:18 -0500

    solve privateattributes

--- privateattributes/solution.py
+++ privateattributes/solution.py
@@ -3,18 +3,21 @@ from private_attributes import secure1, secure2, secure3
 
 def secure1_value(in_object):
     in_object.admin = False  #pyWars.solution() will ignore changing this to True
-    val = in_object.value   #You must retrieve this while admin is False
+    val = in_object._Secure1__the_actual_variable   #You must retrieve this while admin is False
     return val
 
 def secure2_value(in_object):
     in_object.admin = False  #pyWars.solution() will ignore changing this to True
-    val = in_object.value   #You must retrieve this while admin is False
+    val = type(in_object).__dict__['value']._PrivateAttrib__value   #You must retrieve this while admin is False
     return val
 
 def secure3_value(in_object):
     in_object.admin = False  #pyWars.solution() will ignore changing this to True
-    val = in_object.value   #You must retrieve this while admin is False
+    type(in_object).__getattribute__ = object.__getattribute__
+    val = in_object.value
     return val
+
+
 
 print("The value for secure1 is ", secure1_value(secure1))
 
