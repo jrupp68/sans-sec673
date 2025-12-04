ba3ddf7bf4c14507cdcb1abd21955d96fbce8515
sec673 <673@673.local>
2025-12-04 17:37:57 -0500

    solve context-manager

--- context-manager/sniffer/filter.py
+++ context-manager/sniffer/filter.py
@@ -6,7 +6,7 @@ from sniffer.protocols import *
 
 class Filter(object):
     #Add a list or deque as a class attribute to store each instance of a filter that is created. 
-
+    combine_filters = []
     def __init__(self, protocol, field, value):
         if not isinstance(protocol, str):
             raise TypeError("The protocol must be a string")
@@ -51,16 +51,27 @@ class Filter(object):
     #The argument will be the filter instance you are adding
     #This method should append a filter instance to the the class attribute storing all of the instances
     #That class attribute should be added at the top of this class definition
+    @classmethod
+    def add_filter(cls, filter):
+        cls.combine_filters.append(filter)
     
     #Add a new class method called del_filter that takes no arguments
     #This method should delete the last filter instance from the the class attribute storing all of the instances
-    
+    @classmethod
+    def del_filter(cls):
+        cls.combine_filters.pop()
+
     #Add the dunder that is called when a context manager is started
     #It should call add_filter and add the current object (self)
     #This dunder should always return self
+    def __enter__(self):
+        self.add_filter(self)
+        return self
 
     #Add the dunder that is called when a context manager code block is finished
     #This method should call del_filter
+    def __exit__(self, type, value, traceback):
+        self.del_filter()
 
     #Add a method called all_match
     #It takes one argument which will be a Packet() object to check
@@ -68,6 +79,11 @@ class Filter(object):
     #You need a loop going through all of the filters stored in the class atribute
     #If any one of those doesn't match you can return False
     #If you go through the entire list and none of them returned False you can return True
+    def all_match(self, pkt):
+        for other_filter in self.combine_filters:
+            if other_filter != pkt:
+                return False
+        return True
 
     #Add a method called any_match
     #It takes one argument which will be a Packet() object to check
@@ -75,8 +91,15 @@ class Filter(object):
     #You will need a loop to go through our combined filters
     #If the current filter in the loop matches return True
     #If you go through the entire list and none of them returned True you can return False
+    def any_match(self, pkt):
+        for other_filter in self.combine_filters:
+            if other_filter == pkt:
+                return True
+        return False
 
     #Add a method called none_match
     #It takes one argument which will be a Packet() object to check
     #In order to not repeat functionality you can just negate any_match
-    
+    def none_match(self, pkt):
+        return not self.any_match(pkt)
+
