27f36c0f5920883cd6b2b78a95154d7621498cea
sec673 <673@673.local>
2025-12-05 17:18:53 -0500

    solve bonus-decoratorfactory

--- bonus-decoratorfactory/solution.py
+++ bonus-decoratorfactory/solution.py
@@ -1,13 +1,22 @@
 import time
+import collections
 
 
-def decorator_factory():
-    #Complete this decorator as outlined in the read me.
-    #You should calculate elapsed time similar to the following code.
-    start_time = time.process_time()
-    end_time = time.process_time()
-    elapsed_time = end_time - start_time
-    return elapsed_time
+def decorator_factory(num_times, *o_args, **o_kwargs):
+    def mydecorator(func_to_decorate):
+        def newfunc(*args, **kwargs):
+            start_time = time.process_time()
+            all_results = []
+            for _ in range(num_times):
+                combined = dict( **o_kwargs, **kwargs)
+                result = func_to_decorate(*o_args, *args)
+                all_results.append(result)
+            end_time = time.process_time()
+            elapsed_time = end_time - start_time
+            return (all_results, elapsed_time)
+        return newfunc
+    return mydecorator
+
 
 @decorator_factory(5,5,10)
 def mysum(*args):
