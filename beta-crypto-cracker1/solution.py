8e523168390cffbf7467317b106a631b314bbf00
sec673 <673@673.local>
2025-12-05 09:56:11 -0500

    solve pexpect homework

--- beta-crypto-cracker1/solution.py
+++ beta-crypto-cracker1/solution.py
@@ -7,14 +7,34 @@ def all_msb_clear(data:bytes) -> bool:
     #MSB unset on every byte
     return True
 
-def key_length(data:bytes)-> int:
-    #The data contains an encrypted block of data. Lets call that data DATA1. An XOR cypher was used to encrypt DATA1 with KEY1. KEY1 is a fixed length key that is repeated over and over again for the length of DATA1. The clear text data DATA1 was comprised solely of ASCII characters (ie each bytes Most Significant Bit is clear). If you somehow eliminated they key from the data by XORing the data with itself, those bits would be clear again. Remember that if ENC1 = DATA1 ^ KEY1 and ENC2 = DATA2 ^ KEY1 then ENC1^ENC2 = DATA1^DATA2 because KEY1^KEY1 is all zeros. Determine the length of repeating KEY1. This function will return the length of the repeating key.
- 
+def key_length(data: bytes, max_key_len: int = 40) -> int:
+    if len(data) < 20:
+        return 1
+
+    best_len = 2
+    best_score = 0.0
+
+    for length in range(2, max_key_len + 1):
+        matches = 0
+        for i in range(len(data) - length):
+            if (data[i] ^ data[i + length]) < 128:   # MSB cleared → same key byte
+                matches += 1
+        score = matches / (len(data) - length)
+
+        if score > best_score:
+            best_score = score
+            best_len = length
+
+    return best_len
+
 def xor(data:bytes, key:bytes)->bytes:
     #Given data bytes and a key bytes XORs them together and returns the results as bytes. The key may be shorter than the data. In that case the key will be repeated over and over until its length is the same as data.
+    pass
 
 def all_printable(data:bytes)->bool:
     #Given data bytes it will return True if data is comprised 100% of characters in "string.printable". ie the "printable" string in the "string" module. 
+    pass
 
 def check_key(data:bytes, key_size:int, key_to_check:bytes, key_offset:int) -> bool:
-    
+    pass
+
--- pexpect-homework/solution.py
+++ pexpect-homework/solution.py
@@ -3,10 +3,17 @@ import re
 import sys
 
 client = pexpect.spawn('python3 homework.py '+sys.argv[1])
-
+for _ in range(100):
+    client.expect("Hex:")
+    question = client.before
+    binary = re.findall(rb"0b(\d{8})", question)[0]
+    integer = int(binary,2)
+    # answer = hex(integer) doesn't provide padding
+    answer = f"0x{integer:02x}"
+    client.sendline(answer)
 client.expect("\r\n")
-print(client.before)
 client.expect("\r\n")
-print(client.before)
 client.expect("\r\n")
-print(client.before)
+response = client.before
+flag = re.findall(rb"The flag is '(.*?)'", response)[0]
+print(flag.decode())
